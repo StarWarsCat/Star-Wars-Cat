10c5f448b1152f2332d71c7be23b7f5d39e45a64
StarWarsCat <catcat202109@163.com>
2021-09-25 14:55:14 +0800

    erc20

---
+++ contracts/ERC20Token.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+
+contract ERC20Token is ERC20Upgradeable {
+
+    function __ERC20Token_init() public initializer {
+        __ERC20_init("Star Wars Cat", "SWCAT");
+
+        _mint(msg.sender, 100 * 10**8 * 10**decimals());
+    }
+
+    function decimals() public view override returns (uint8) {
+        return 9;
+    }
+
+}
